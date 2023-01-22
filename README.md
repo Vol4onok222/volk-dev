f4e925a7575fd19dbd6f66e88aeb0f4d5bb9975b
Vol4onok222 <108519849+Vol4onok222@users.noreply.github.com>
2023-01-22 09:06:58 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# volk-dev
-Portfolio
+<div align="center"> <h1> Portfolio- Website</h1>
+<p>A personal portfolio website built with HTML, CSS, and JavaScript to make Open Source accessible to newcomers.</p></div>
+
+## Demo
+![portfolio](https://user-images.githubusercontent.com/107752425/192083103-e773c142-6ed0-4223-8fca-62525c2f908a.png)
+
+## Chat with me
+
+Want to chat with me? Join my discord server from below badge.
+
+[![Join now - Discord](https://img.shields.io/badge/Join_now-Discord-2ea44f?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/CapGk4dEaV)
+
+------
+
+<div align="center">
+<h3>Don't forget to ⭐ this repo</h3>
+</div>
---
+++ assets/projects.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    title: "Sharkfolio",
+    description:
+      "Сайт-портфолио акул. Во время хакатона MLH он был построен с использованием React, Tailwind CSS и GSAP.",
+    image:
+      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+    github: "https://github.com/Valexr/harkfolio",
+    live: "https://sharkfolio.vercel.app/",
+  },
+  {
+    title: "Notty",
+    description:
+      "Аналогичная версия тематики - Заметок. Сделанная под определенный ключ и оптимизорованную переадресацию DRT процессоров.",
+    image:
+      "https://i.postimg.cc/FHkTRg4T/image.png",
+    github: "https://github.com/Valexr/Notty",
+    live: "https://notty.surge.sh/",
+  },
+  {
+    title: "Портфолио USA",
+    description:
+      "Веб-сайт с портфолио, созданный с использованием HTML, CSS и JavaScript. Я сделал опнесорс во время хактоберфеста",
+    image:
+      "https://i.postimg.cc/c4RQ59dy/image.png",
+    github: "https://github.com/Valexr/portflio",
+    live: "http://alexaf9d.beget.tech/shablon/usa/index.html",
+  },
+  {
+    title: "Bankist",
+    description:
+      "Bankist — это онлайн-платформа, где пользователь может получить доступ к реквизитам своего банковского счета, перевести деньги и многое другое.",
+    image:
+      "https://user-images.githubusercontent.com/107752425/205921327-16fa9da1-d602-4b18-8cce-6f80c4585fd2.png",
+    github: "https://github.com/bankist-application",
+    live: "https://bankist-application.vercel.app",
+  },
+  {
+    title: "Slidy",
+    description:
+      "Динамичная версия Slidy - Сделанная исключетельно под ключ. С использованием JavaScript и С#",
+    image:
+      "https://raw.githubusercontent.com/Valexr/Slidy/master/assets/static/flip.gif",
+    github: "https://github.com/Valexr/Slidy",
+    live: "https://www.rakeshsangem.me",
+  },
+    {
+    title: "Comparison component",
+    description:
+      "Автоматическая и аналочная версия редактора специальных эффектов. Специальная разработка производилась для Компании - GSAP NEWS",
+    image:
+      "https://raw.githubusercontent.com/Valexr/Compary/master/Screenshot.jpg",
+    github: "https://github.com/Valexr/Compary",
+    live: "https://valexr.github.io/Compary/",
+  },
+    {
+    title: "15-puzzle game",
+    description:
+      "Копия популярной Android игры - 15 Puzzle. Совмещенная с Adminer'om 6.4. Для работы требовалась так же статичная GET структура.",
+    image:
+      "https://i.postimg.cc/Ghxyj4jb/image.png",
+    github: "https://github.com/Valexr/Notty",
+    live: "https://tagy.surge.sh/",
+  },
+];
